222bd5194825ac7da5538056c5274e837af87bad
Ali Osmar <63081921+aliosmarchehade@users.noreply.github.com>
2024-10-04 10:49:50 -0300

    Update Instrucoes.java

--- simulador/src/Instrucoes.java
+++ simulador/src/Instrucoes.java
@@ -4,6 +4,7 @@ public class Instrucoes {
     public short format;
     public short opcode;
     public short destino;
+    public short registradorr;
     public short operando1;
     public short operando2;
     public short imediato;
