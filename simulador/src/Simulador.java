098d4e172f1fc26b07cb886a3fcda8b51ccd2f3a
Ali Osmar <63081921+aliosmarchehade@users.noreply.github.com>
2024-10-04 10:48:49 -0300

    Update Simulador.java

--- simulador/src/Simulador.java
+++ simulador/src/Simulador.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 
 public class Simulador {
 
-	private static int MEMORY_SIZE = 1024;	
+	public static int MEMORY_SIZE = 1024;	
 
-	private short[] memory = new short[MEMORY_SIZE];
-	private short[] registradores = new short[8];
+	public short[] memory = new short[MEMORY_SIZE];
+	public short[] registradores = new short[8];
 
-	public static String nomeRegistradores(int registradores){
+	/* public static String nomeRegistradores(int registradores){
 		String[] registradoresNome = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7"};
 		return registradoresNome[registradores];
-	}
+	} */
 
 	public short extract_bits (short value, int bstart, int blength){
 		short mask = (short)((1 << blength) - 1);
@@ -38,12 +38,13 @@ public class Simulador {
 		//instruções para o formato I:
 		else if(decodificada.format == 1) {
 			decodificada.opcode = extract_bits(instrucaoCodificada, 13, 2);
+			decodificada.registradorr = extract_bits(instrucaoCodificada, 10, 3);
 			decodificada.imediato = extract_bits(instrucaoCodificada, 0, 10);
 		}
 		return decodificada; 	
 	}
 
-	public void executar(Instrucoes instrucoes){
+	public boolean executar(Instrucoes instrucoes){
 		if (instrucoes.format == 0){
 			if(instrucoes.opcode == 0){ //add
 				registradores[instrucoes.destino] = (short) (registradores[instrucoes.operando1] + registradores[instrucoes.operando2]);
@@ -75,20 +76,32 @@ public class Simulador {
 			}
 			else if(instrucoes.opcode == 15){ //load
 				short endereco = registradores[instrucoes.operando1];
-				registradores[instrucoes.destino] = memory[endereco]; //armazena no registrador um valor da memória
+				registradores[instrucoes.destino] = memory[endereco];
+				 //armazena no registrador um valor da memória
 
-				registradores[instrucoes.destino] = memory[registradores[instrucoes.operando1]];
 			}
-			else if(instrucoes.opcode == 7){ //div
-				registradores[instrucoes.destino] = (short) (registradores[instrucoes.operando1] / registradores[instrucoes.operando2]);
+			else if(instrucoes.opcode == 16){ //store
+				memory[registradores[instrucoes.operando1]] = registradores[instrucoes.destino];
+				//armazena na memoria um valor de um registrador
+			}
+
+			else if(instrucoes.opcode == 63){ //syscall para fechar
+				if(registradores[0] == 0){
+					System.out.println("Encerrando ");
+					return false;
+				}
+				
+			}
 			
-			else if(instrucoes.opcode == 8){ //div
-				registradores[instrucoes.destino] = (short) (registradores[instrucoes.operando1] / registradores[instrucoes.operando2]);
-			}
-			else if(instrucoes.opcode == 9){ //div
-				registradores[instrucoes.destino] = (short) (registradores[instrucoes.operando1] / registradores[instrucoes.operando2]);
+		}
+		else if(instrucoes.format == 1){
+			if(instrucoes.opcode == 3){
+				registradores[instrucoes.registradorr] = instrucoes.imediato;
+				//System.out.println(registradores[instrucoes.registradorr]);
+				//System.out.println(instrucoes.registradorr);
 			}
 		}
+		return true;
 	}
 		
 	void load_binary (String binary_name)
